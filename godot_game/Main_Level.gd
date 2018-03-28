83b7c06c8d32b740a2c31cbaef572c41885a07cb
Diogo Botas <gamebreak24@gmail.com>
2018-03-28 11:40:28 +0100

    LMB and R processed correctly. Reload sound added. HUD works perfectly.

--- godot_game/Main_Level.gd
+++ godot_game/Main_Level.gd
@@ -4,49 +4,27 @@ export (PackedScene) var enemy
 export (int) var kill_score = 50
 var score
 var bullets = 6
-var ev
 
 func _ready():
 	score = 0
 	$Player.start($Position2D.position)
 	pass
 
-func kill_timer_timeout():
-	score += kill_score
-	$HUD.update_score(score)
-	pass
-
-func _process(delta):
-	if(Input.is_key_pressed(KEY_R) && bullets!=6):
-		bullets = 6
-		$HUD.update_ammo(bullets)
-	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
-#		$Audio/Shooting.play(0)
-		$Kill_Timer.start()
-#		if(bullets != 0):
-#			bullets = bullets - 1
-#			$HUD.update_ammo(bullets)
-	pass
-
-func game_over():
-	pass
-
-func _on_Enemy_Timer_timeout():
-#	$Enemy_Path/Path_Follow.set_offset(randi())
-#	var mob = enemy.instance()
-#	add_child(mob)
-#	var direction = $Enemy_Path/Path_Follow.rotation + PI/2
-#	mob.position = $Enemy_Path/Path_Follow.position
-#	direction += rand_range(-PI/4, PI/4)
-#	mob.rotation = direction
-#	mob.set_linear_velocity(Vector2(rand_range(10, 100), 0).rotated(direction))
-	pass
-
 func _on_Start_Timer_timeout():
 	$Enemy_Timer.start()
 	pass
 
-
-func shooting_sound_player(viewport, event, shape_idx):
-	$Audio/Shooting.play(0)
+func _input(event):
+	if(event.is_action_pressed("left_mouse_click")):
+		if(bullets != 0):
+			$Audio/Shooting.play(0)
+			score += kill_score
+			$HUD.update_score(score)
+			if(bullets != 0):
+				bullets = bullets - 1
+				$HUD.update_ammo(bullets)
+	if(event.is_action_pressed("reload_key")):
+		bullets = 6
+		$HUD.update_ammo(bullets)
+		$Audio/gun_reload.play(0)
 	pass
